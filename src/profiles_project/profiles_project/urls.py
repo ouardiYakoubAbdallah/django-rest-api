5e2d3ee89e4e6a454387aa965fdcf118b085f550
OUARDI Yakoub Abdallah <ouardi.yakoubabdallah@gmail.com>
2020-10-17 23:00:37 +0100

    Create the first APIView

--- src/profiles_project/profiles_project/urls.py
+++ src/profiles_project/profiles_project/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from profiles_api import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('first/', views.FirstView.as_view(), name='My first view')
 ]
