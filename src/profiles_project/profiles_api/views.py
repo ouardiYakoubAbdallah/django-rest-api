9c6352b287a3c70b5e969f1e904f5921f542142c
OUARDI Yakoub Abdallah <ouardi.yakoubabdallah@gmail.com>
2020-10-19 14:34:59 +0100

    Add authentification and create permissions

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -3,9 +3,12 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
 from . import serializers
 from . import models
+from . import permissions
+
 
 # Create your views here.
 class UserProfileViewSet(viewsets.ModelViewSet):
@@ -15,3 +18,6 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+
